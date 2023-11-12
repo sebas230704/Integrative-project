f01175a552d3f67fa5b159b56c187816acaac8e0
Juan Diego <jdllorento@eafit.edu.co>
2023-11-12 17:25:53 -0500

    Estilos en formularios

--- Eventix/djangoproject/settings.py
+++ Eventix/djangoproject/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eventix',
+        'NAME': 'eventix_db',
         'USER': 'root',
-        'PASSWORD': 'sebasgamer1',
+        'PASSWORD': 'root',
         'HOST': 'localhost',  # Puede variar según la configuración de tu servidor MySQL
         'PORT': '3306',           # Deja en blanco para usar el puerto predeterminado
     }
